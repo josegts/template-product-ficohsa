05e8afa6984adf492f5ea6981781dcd6c5e38904
Jose Gutierrez <jocgts@gmail.com>
2023-11-18 22:02:13 -0500

    feat: mf test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    const { isServer } = options
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'product',
+        remotes: {
+          navbar: `navbar@http://localhost:3001/remoteEntry.js`,
+          documents: `documents@http://localhost:3002/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+        },
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {},
+        shared:  [ 'react', 'react-dom', '@emotion/react' ],
+      })
+    )
+    return config
+  }
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
 import { Button, Typography } from '@josegts/ds-components';
+import { Toolbar } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import dynamic from 'next/dynamic';
 import { Trans } from 'react-i18next';
+
+const NavbarMf = dynamic(async () => await import('navbar/NavbarMf' as any), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
+
+/* const NavbarMf = dynamic(async () => await import('navbar/NavbarMf'), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+  webpack: (config, options) => {
+    config.experiments = { topLevelAwait: true };
+    // if failer show message
+    config.optimization.runtimeChunk = false;
+    config.optimization.splitChunks = {
+      cacheGroups: {
+        default: false,
+      },
+    };
+    return config;
+  },
+}); */
 
 const Home = () => {
   /* const mutation = useMutation<any, any, any, any>({
@@ -26,17 +49,21 @@ const Home = () => {
   );
   return (
     <div>
-      <Button variant='elevated'>Elevated</Button>
-      <Typography variant='h1'>
-        <Trans i18nKey='featureExample.titleExample' />
-      </Typography>
-      <Typography variant='h2'>
-        <Trans i18nKey='featureExample.subtitleExample' />
-      </Typography>
-      <Typography>Fetch with tanstack query</Typography>
-      {data?.data?.data?.map((todo: any) => (
-        <Typography key={todo.id}>{todo.name}</Typography>
-      ))}
+      <NavbarMf />
+      <div>
+        <Toolbar sx={{ height: { xs: '80px', lg: '145px' } }} />
+        <Button variant='elevated'>Elevated</Button>
+        <Typography variant='h1'>
+          <Trans i18nKey='featureExample.titleExample' />
+        </Typography>
+        <Typography variant='h2'>
+          <Trans i18nKey='featureExample.subtitleExample' />
+        </Typography>
+        <Typography>Fetch with tanstack query</Typography>
+        {data?.data?.data?.map((todo: any) => (
+          <Typography key={todo.id}>{todo.name}</Typography>
+        ))}
+      </div>
     </div>
   );
 };
